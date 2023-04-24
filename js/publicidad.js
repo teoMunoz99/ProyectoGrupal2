44e5613a437d447127ffa8e39768459d6b9a14bc
Marianjuavi <mariana.ju92@gmail.com>
2023-04-24 10:20:32 -0300

    Se modifica nombre de publicidad a cartel publicitario y se sube el codigo de la funcion que estaba incompleto

--- js/publicidad.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function () {
-    document.getElementById("publicidadModal").style.display = "block";
-}, 4000);
-
-document.getElementById("cerrarPublicidad").addEventListener("click", function () {
-    document.getElementById("publicidadModal").style.display = "none";
-});
